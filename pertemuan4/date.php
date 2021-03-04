c8431720ffbe0029c2b2adadbc30396cceb1f973
fdlashari <asharifadil19@gmail.com>
2021-03-04 15:53:54 +0700

    penyelesaian tugas

---
+++ pertemuan4/date.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Muhammad Fadhil Ashari
+203040148
+*/
+
+// Date
+// Untuk menampilkan tanggal dengan format tertentu
+// echo date("l, d-M-Y");
+
+// TIME
+// UNIX Timestamp / EPOCH time
+// detik yag sudah berlalu sejak 1 Januari 1970
+// echo time();
+// echo date("l", time()-60*60*24*100);
+
+// mktime
+// membuat sendiri detik
+// mktime (0,0,0,0,0,0,)
+// jam, menit, detik, bulan, tanggal, tahun
+// echo date("l", mktime(0,0,0,8,25,1985));
+
+// strtotime
+echo date("l", strtotime("25 aug 1985"));
+?>
