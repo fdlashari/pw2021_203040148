854701522cdb240b3098f55433d48012316b972a
fdlashari <asharifadil19@gmail.com>
2021-03-18 16:52:09 +0700

    penyelesaian tugas

---
+++ praktikum/Tugas3/latihan3a.php
@@ -0,0 +1,10 @@
+<!-- 
+Muhammad Fadhil Ashari
+203040148
+-->
+
+<?php
+
+$kata = ["ada", "abel", "men", "pung", "nilai"];
+
+echo "Array $kata[0]lah suatu vari$kata[1] yang dapat $kata[2]am$kata[3] banyak $kata[4]";
