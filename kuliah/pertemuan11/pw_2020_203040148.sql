ed1624dae22bbe4eafae98c00ad08f8ed28f08a6
fdlashari <asharifadil19@gmail.com>
2021-05-20 19:00:26 +0700

    penyelesaian tugas

--- kuliah/pertemuan11/pw_2020_203040148.sql
+++ kuliah/pertemuan11/pw_2020_203040148.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2021 at 10:43 AM
+-- Generation Time: May 20, 2021 at 01:46 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -59,6 +59,15 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'admin', '12345'),
+(2, 'admin', '12345'),
+(3, 'fadil ', '$2y$10$/Z17I30PkavI6Rs7vY5DtuEWiPC8yrYcQS6nOnLeuTdo9Yd7aa.1m');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -88,7 +97,7 @@ ALTER TABLE `mahasiswa`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
